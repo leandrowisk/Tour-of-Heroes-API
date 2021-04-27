7297cc43909a8186e161ea339ad19eb5a85339dd
Leandro Tecioni <leandrotdrumond@gmail.com>
2021-04-27 09:30:34 -0300

    parte4-criando-o-model-da-coleção-heroi

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from flask_cors import CORS
 import firebase_admin
 from firebase_admin import firestore
 
-
 # start da  API
 app = Flask(__name__)
 CORS(app)
---
+++ modules/main.py
@@ -0,0 +1,12 @@
+from firebase_admin import firestore
+
+
+class MainModule(object):
+    """Main module"""
+
+    # Vamos criar esse metodo somente para ficar mais facil fazer o mock dele
+    # nos testes
+    @staticmethod
+    def get_firestore_db():
+        """Get firestore db instance"""
+        return firestore.client()
